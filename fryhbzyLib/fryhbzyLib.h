30d41a9e4fcc658158c6d51fb9ca40edc1587f57
bzy <bzy@fuliangfeng-mini.local>
2017-04-14 15:00:07 +0800

    change

---
+++ fryhbzyLib/fryhbzyLib.h
@@ -0,0 +1,13 @@
+//
+//  fryhbzyLib.h
+//  fryhbzyLib
+//
+//  Created by bzy on 17/4/14.
+//  Copyright © 2017年 bzy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface fryhbzyLib : NSObject
+
+@end
---
+++ fryhbzyLib/fryhbzyLib.m
@@ -0,0 +1,13 @@
+//
+//  fryhbzyLib.m
+//  fryhbzyLib
+//
+//  Created by bzy on 17/4/14.
+//  Copyright © 2017年 bzy. All rights reserved.
+//
+
+#import "fryhbzyLib.h"
+
+@implementation fryhbzyLib
+
+@end
